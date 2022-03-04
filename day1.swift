c928df88fbc2d5faea3913599b4d3c9d533dd871
Daniel Braga <dbragabarbosa@gmail.com>
2022-03-04 17:05:42 -0300

    Finalizando o day1.swift

--- day1.swift
+++ day1.swift
@@ -1,3 +1,40 @@
 // Aprendi sobre variáveis e como o próprio Swift tipa minhas novas variáveis 
 // como inteiro ou string de acordo com o valor que passo de entrada.
-// PAREI NO 3. STRINGS DE VÁRIAS LINHAS
+
+// Aprendi a escrever strings de várias linhas para facilitar a leitura no código:
+// var str1 = """"
+// This goes 
+// over multiple
+// lines
+// """
+
+// Aprendi sobre o tipo de dado double, que é tipado pra armazenar números decimais e 
+// o tipo de dado booleano que armazena true ou false
+
+// Aprendi sobre interpolação de strings do tipo:
+// var score = 85
+// var str = "Your score was \(score)"
+
+// Aprendi a usar let para criar uma constante, ou seja,
+// uma variável que não quero que muda nunca:
+// let taylor = "swift"
+
+// Posso ser explícito na cariação dos meus tipos de dados dessa forma:
+// let album: String = "Reputation"
+// let year: Int = 1989
+// let height: Double = 1.78
+// let taylorRocks: Bool = true
+
+// RESUMO:
+
+// 1. You make variables using var and constants using let. It’s preferable to use constants as often as possible.
+
+// 2. Strings start and end with double quotes, but if you want them to run across multiple lines you should use three sets of double quotes.
+
+// 3.Integers hold whole numbers, doubles hold fractional numbers, and booleans hold true or false.
+
+// 4.String interpolation allows you to create strings from other variables and constants, placing their values inside your string.
+
+// 5.Swift uses type inference to assign each variable or constant a type, but you can provide explicit types if you want.
+
+// Terminei o day1: 04/03/2022
