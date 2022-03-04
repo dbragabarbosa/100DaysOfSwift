b03b6d4db6e04f65b07abc7b460caa0362b6534a
Daniel Braga <dbragabarbosa@gmail.com>
2022-03-04 00:06:26 -0300

    Atualizando o day1.swift

--- day1.swift
+++ day1.swift
@@ -1,2 +1,3 @@
-var teste: Int = 5
-print(teste)
+// Aprendi sobre variáveis e como o próprio Swift tipa minhas novas variáveis 
+// como inteiro ou string de acordo com o valor que passo de entrada.
+// PAREI NO 3. STRINGS DE VÁRIAS LINHAS
