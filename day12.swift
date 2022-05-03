116502c3e646da547b3779ad4015c8b89b7a12b8
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-02 22:57:07 -0300

    Day12.swift

--- day12.swift
+++ day12.swift
@@ -1,2 +1,16 @@
-25/03/2022
+29/04/2022
+
+
+OPTIONALS (opcionais)
+
+Referências nula - literalmente quando uma variável não tem valor - foram inventadas por Tony Hoare em 1965. 
+Quando perguntado sobre isso em retrospecto, ele disse: "Eu chamo isso de meu erro de bilhões de dólares" porque eles levam a muitos problemas.
+
+Este dia é dedicado exclusivamente à solução da Swift para referências nulas, conhecidas como opcionais. 
+Estes são um recurso de linguagem muito importante, mas podem machucar um pouco o seu cérebro.
+
+
+---------- 1. HANDLING MISSING DATA 
+
+
 
