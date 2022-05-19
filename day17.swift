c822cead0ceaa1624141b6685d39bde5543a7323
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-19 01:33:50 -0300

    Day17.swift

--- day17.swift
+++ day17.swift
@@ -1,2 +1,18 @@
 // 18/05/2022
 
+
+PROJECT 1, PART TWO 
+
+Ontem você completou o básico do nosso aplicativo, mas é claro que está faltando um componente importante: 
+ele não está desenhando nenhuma imagem! Para um aplicativo chamado Storm Viewer, essa parte parece bastante importante - 
+como disse Walt Disney, "de todas as nossas invenções para comunicação de massa, as imagens ainda falam a linguagem mais 
+universalmente compreendida".
+
+Então, hoje você concluirá o projeto um adicionando uma tela de detalhes que pode carregar imagens, fazendo com que essa tela seja 
+animada quando o usuário selecionar um nome de imagem e, em seguida, adicionando alguns ajustes para polir a interface do usuário.
+
+Today you have three topics to work through, and you’ll meet UIImageView, UIImage, UINavigationBar, and more.
+
+
+---------- 1. BUILDING A DETAIL SCREEN 
+
