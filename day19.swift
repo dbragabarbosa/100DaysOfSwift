d2a896d4a262e09a0e73f0a88fda434a11d1d1cf
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-25 15:48:15 -0300

    Day19.swift

---
+++ day19.swift
@@ -0,0 +1,15 @@
+25/05/2022
+
+
+PROJECT 2, PART ONE 
+
+Santo Agostinho disse que "o mundo é um livro, e aqueles que não viajam leem apenas uma página". 
+Se você é como eu, gosta muito de viajar, então, neste projeto, vamos fazer um pequeno jogo que incentiva 
+as pessoas a aprender mais sobre o mundo ao seu redor.
+
+Sim, você leu corretamente: estamos fazendo um jogo. Não se preocupe - você ainda estará usando Swift e UIKit 
+e aprenderá pilhas de habilidades importantes que funcionam tão bem em aplicativos quanto em jogos.
+
+
+---------- 1. 
+
