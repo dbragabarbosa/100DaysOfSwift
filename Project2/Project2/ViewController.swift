770e466628e9b28fafc25ddaa60f76b4ee26fc48
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-25 17:10:22 -0300

    Day19.swift

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Daniel Braga Barbosa on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
