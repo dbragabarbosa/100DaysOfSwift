31801ace339f2f9bf4e816dba12fdc651b5f8aaa
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-28 02:56:26 -0300

    Day21.swift

---
+++ day21.swift
@@ -0,0 +1,24 @@
+24/05/2022
+
+
+PROJECT 2, PART THREE
+
+Agora que nosso aplicativo está completo, é hora de uma rápida recapitulação do que você aprendeu, 
+um pequeno teste para ter certeza de que entendeu o que foi ensinado e, em seguida, outro conjunto de desafios - 
+exercícios projetados para que você escreva seu próprio código o mais rápido possível.
+
+Eu não forneço as respostas para esses desafios. Isso é intencional: eu quero que você descubra sozinho, em vez de 
+apenas olhar para o trabalho de outra pessoa.
+
+John Carmack disse uma vez: “o trabalho focado e duro é a verdadeira chave para o sucesso. Fique de olho no 
+objetivo e continue dando o próximo passo para concluí-lo. Se você não tem certeza de qual maneira fazer algo, 
+faça nos dois sentidos e veja qual funciona melhor.”
+
+Então, espero que você concorde que tentar as coisas por si mesmo, cometer erros e encontrar soluções é a chave para o aprendizado.
+
+Hoje você deve trabalhar no capítulo de encerramento do projeto 2, concluir sua revisão e, em seguida, 
+trabalhar em todos os seus três desafios.
+
+
+---------- 1. WRAP UP 
+
