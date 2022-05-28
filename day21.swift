63dc89c10b0ab36e6e1bd4bebba722c5a4681036
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-28 03:10:10 -0300

    Day21.swift

--- day21.swift
+++ day21.swift
@@ -22,3 +22,36 @@ trabalhar em todos os seus três desafios.
 
 ---------- 1. WRAP UP 
 
+Este foi outro projeto relativamente simples, mas deu a você a chance de revisar alguns conceitos com um pouco 
+mais de detalhes, ao mesmo tempo em que aperta mais alguns conceitos ao lado. Revisar as coisas novamente de uma 
+maneira diferente é sempre útil para aprender, então espero que você não veja este jogo (ou qualquer um dos jogos 
+que faremos nesta série!) como uma perda de tempo.
+
+Yes, in this project we revisited Interface Builder, Auto Layout, outlets and other things, but at the same time you've 
+learned about @2x and @3x images, asset catalogs, UIButton, CALayer, UIColor, random numbers, actions, UIAlertController, 
+and more. And you have a finished game too!
+
+Revise o que você aprendeu
+
+Qualquer um pode assistir a um tutorial, mas é preciso trabalho real para lembrar o que foi ensinado. 
+É meu trabalho garantir que você aproveite o máximo possível desses tutoriais, então preparei uma breve 
+revisão para ajudá-lo a verificar seu aprendizado.
+
+Desafio
+
+Uma das melhores maneiras de aprender é escrever seu próprio código o mais rápido possível, então aqui 
+estão três maneiras pelas quais você deve tentar estender este aplicativo para garantir que você entenda c
+ompletamente o que está acontecendo:
+
+1. Tente mostrar a pontuação do jogador na barra de navegação, ao lado da bandeira para adivinhar.
+
+2. Acompanhe quantas perguntas foram feitas e mostre um controlador de alerta final depois que eles 
+responderem a 10. Isso deve mostrar sua pontuação final.
+
+3. Quando alguém escolher a bandeira errada, diga a ele o erro na sua mensagem de alerta - algo como 
+"Errado! Essa é a bandeira da França", por exemplo.
+
+
+
+---------- 2. REIVEW FOR PROJECT 2: GUESS THE FLAG 
+
