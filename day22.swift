7013001bcb92ae0e667eecc520c1b48009af5b97
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-30 02:29:28 -0300

    Day22.swift

--- day22.swift
+++ day22.swift
@@ -3,5 +3,39 @@
 
 PROJECT 3
 
+O famoso autor brasileiro Paulo Coelho disse: “O Twitter é o meu bar: eu me sento no balcão e ouço as conversas, 
+iniciando outras, sentindo a atmosfera.” Quer você use muito ou não, as mídias sociais são uma enorme força 
+motriz para a mudança em nosso mundo.
 
----------- 1. 
+Para os desenvolvedores de aplicativos, as mídias sociais também são uma chance para nossos usuários falarem 
+sobre nosso aplicativo: compartilhar coisas que encontraram, ou talvez apenas dizer ao mundo o quanto eles adoram. 
+Isso ajuda a quebrar a sensação de que nosso aplicativo está contido no dispositivo deles - se pudermos ajudá-los 
+a se comunicar com amigos on-line, eles nos agradecerão por isso.
+
+Então, hoje você vai conhecer seu primeiro projeto de técnica: um projeto especificamente dedicado ao ensino de 
+uma habilidade específica do iOS ou Swift. Isso às vezes envolve novos projetos, e outras vezes envolve a mudança 
+de projetos existentes, mas você sempre aprenderá algo novo.
+
+Today you have three topics to work through, and you’ll meet UIBarButtonItem and UIActivityViewController. 
+Once that’s done, complete the chapter review, then work through all three of its challenges.
+
+
+---------- 1. ABOUT TECHNIQUE PROJECTS 
+
+Como você deve saber, esta série segue o aplicativo de pedidos, o jogo, a técnica. O Project 1 era um 
+aplicativo que permitia os usuários navegarem por imagens em seus telefones, o Project 2 era um jogo que 
+permite que os jogadores adivinharem bandeiras, então agora é hora do primeiro projeto de técnica.
+
+O objetivo dos projetos técnicos é escolher uma tecnologia iOS e se concentrar nela em profundidade. 
+Alguns serão fáceis, outros nem tanto, mas prometo tentar mantê-los o mais curto possível, porque sei 
+que você quer se concentrar em fazer coisas reais.
+
+Este primeiro projeto de técnica vai ser muito simples, porque vamos modificar o projeto 1 para fazer algo 
+que ele não faz atualmente: permitir que os usuários compartilhem imagens com seus amigos.
+
+
+
+---------- 2. UIActivityViewController explained 
+
+
+
