5b6a1ca6dd4b47a5b613a1d2124ba530169348ab
Daniel Braga <dbragabarbosa@gmail.com>
2022-06-01 00:49:11 -0300

    Day23.swift

---
+++ day23.swift
@@ -0,0 +1,25 @@
+31/05/2022
+
+
+MARCO: PROJETOS 1-3
+
+É hora de mais um dia de consolidação, porque cobrimos muito terreno nos três primeiros tópicos e é importante 
+que você os revise se quiser que eles fiquem na sua cabeça.
+
+No entanto, este também será o primeiro dia em que você será solicitado a criar um aplicativo completo do zero. 
+Não se preocupe: eu descrevo todos os componentes necessários para fazê-lo funcionar e também forneço dicas para 
+lhe dar uma vantagem.
+
+Como você verá, criar um aplicativo do zero é uma experiência muito diferente de adicionar modificações a um 
+aplicativo existente: você tem paralisia de página em branco, que é onde seu cérebro sabe onde você quer chegar, 
+mas você simplesmente não tem certeza de como começar.
+
+Uma razão comum para ficar preso é que as pessoas tentam escrever código impecável pela primeira vez. Como Margaret 
+Atwood disse uma vez, "se eu esperasse pela perfeição, nunca escreveria uma palavra". Então, mergulhe e veja onde 
+você está - esses desafios marcantes ajudarão você a aprender a se sentir confortável iniciando novos projetos e a 
+colocar a funcionalidade real em funcionamento rapidamente.
+
+
+
+---------- 1. WHAT YOU LEARNED 
+
