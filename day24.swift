c24ca1fb1ee6ecad7bafb83aa8283fb7cebf8ef4
Daniel Braga <dbragabarbosa@gmail.com>
2022-06-15 00:13:44 -0300

    Day24.swift

---
+++ day24.swift
@@ -0,0 +1,21 @@
+15/06/2022
+
+
+PROJECT 4, PART ONE 
+
+Alexis Ohanian, fundador do Reddit, disse uma vez que "para se juntar à revolução industrial, você precisava 
+abrir uma fábrica; na revolução da Internet, você precisa abrir um laptop". Bem, graças ao iOS, temos algo ainda 
+mais fácil: você só precisa tocar em um botão no seu iPhone.
+
+A Apple nos dá a capacidade de renderizar qualquer tipo de conteúdo da web, assim como o Safari, tudo alimentado 
+por meio de sua estrutura WebKit de código aberto. Isso é multiplataforma, o que significa que podemos usá-lo no 
+macOS e iOS da mesma forma, e também é incrivelmente rápido, como você verá em um momento.
+
+Neste projeto, vamos criar um navegador da web simples usando o WebKit. A coisa toda leva apenas cerca de 60 
+linhas de código depois que você remove comentários e linhas vazias, o que mostra o quão fácil é usar o WebKit.
+
+Hoje você tem três tópicos para trabalhar e conhecerá o WKWebView, folhas de ação e muito mais.
+
+
+---------- 1. SETTING UP 
+
