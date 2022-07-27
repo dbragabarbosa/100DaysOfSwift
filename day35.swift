202f50475edee5aab9f61489435c2bb61a28042a
Daniel Braga <dbragabarbosa@gmail.com>
2022-07-27 19:05:34 -0300

    Day35.swift

--- day35.swift
+++ day35.swift
@@ -4,4 +4,12 @@
 PROJECT T, PART THREE 
 
 
----------- 
+---------- WRAP UP 
+
+À medida que sua habilidade Swift aumenta, espero que você esteja começando a sentir que 
+o equilíbrio desses projetos se afasta da explicação do básico para apresentar e dissecar código.
+
+In this project you learned how to download JSON using Swift’s Data type, then use the Codable 
+protocol to convert that data into Swift objects we defined. Working with JSON is something you're 
+going to be doing time and time again in your Swift career, and you've cracked it in about an hour 
+of work – while also learning about UITabBarController, UIStoryboard, and more.
