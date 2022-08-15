abe6ea1199c38527c18c6e830276bdf4b17f2b2f
Daniel Braga <dbragabarbosa@gmail.com>
2022-08-15 09:34:10 -0300

    Day37.swift

--- day37.swift
+++ day37.swift
@@ -4,7 +4,27 @@
 PROJECT 8, PART TWO 
 
 
+Como você se saiu com a codificação da interface do usuário de ontem? Depois de concluir os tópicos, houve uma coisa que 
+eu pedi que você tivesse em mente: se você prefere fazer interfaces de usuário em storyboards ou programaticamente, é 
+importante que você saiba como fazer as duas coisas. Agora que você fez as duas coisas, como você se sente sobre isso?
 
+Esta não é apenas uma pergunta teórica - se você é como eu, é uma pergunta que lhe será muito feita, por isso é importante 
+pensar cuidadosamente sobre sua resposta.
+
+Quanto a mim, minha resposta é bem simples: eu faço o que faz sentido em cada cenário individual. Às vezes isso significa 
+usar código, às vezes significa usar o Interface Builder, e às vezes significa usar ambos - e tudo bem. O principal é que 
+você se lembra das palavras de John Woods: "sempre codifique como se a pessoa que acaba mantendo seu código fosse um psicopata 
+violento que sabe onde você mora".
+
+Se isso está tornando sua interface do usuário em storyboards ou em código é acadêmico, na verdade - o que importa é 
+que foi a melhor escolha que você poderia fazer para esse problema, dadas as restrições em que você estava trabalhando.
+
+Você ficará feliz em ver o trabalho duro de ontem valer a pena hoje, porque concluiremos nosso jogo em menos de uma hora 
+de trabalho. Lançamos todas as bases ontem, então hoje vamos olhar para os níveis de carregamento e responder aos toques 
+de botões - chega de Layout Automático por enquanto!
+
+Hoje você tem três tópicos para trabalhar e aprenderá a adicionar alvos a um botão, separar e juntar strings, 
+ocultar visualizações e muito mais.
 
 
 ---------- LOADING A LEVEL AND ADDING BUTTON TARGETS 
