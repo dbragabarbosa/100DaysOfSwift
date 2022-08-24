9f0131bb0bb6b22de2fd55d0e0ab1070f587fdaf
Daniel Braga <dbragabarbosa@gmail.com>
2022-08-24 00:26:41 -0300

    Day38.swift

---
+++ day38.swift
@@ -0,0 +1,23 @@
+24/08/2022 
+
+
+PROJECT 8, PART THREE 
+
+
+Há muitas citações bem conhecidas de Shakespeare, mas há uma que eu acho particularmente adequada hoje: "o tolo pensa 
+que é sábio, mas o sábio sabe que é um tolo". Eu não sei onde você se classificaria em termos de conhecimento de Layout 
+Automático, mas espero que você esteja pelo menos ciente de que é um espaço muito grande e complexo para se trabalhar!
+
+Na minha palestra na NSSpain 2018, eu disse "O layout automático facilita as coisas difíceis e as coisas fáceis" - você 
+descobrirá que pode fazer layouts relativamente avançados como o de hoje em cerca de uma hora, mas ocasionalmente você 
+se encontrará querendo uma restrição de layout específica que é realmente difícil de acertar.
+
+Felizmente para todos nós, esta parte do seu aprendizado de Layout Automático está completa, então é hora de revisarmos 
+o que você aprendeu.
+
+Hoje você deve trabalhar no capítulo de encerramento do projeto 8, concluir sua revisão e, em seguida, trabalhar em todos 
+os três desafios.
+
+
+
+---------- 
