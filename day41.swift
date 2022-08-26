553c2e94bcb5085ba5d65e5b1fb30a37f13caeaf
Daniel Braga <dbragabarbosa@gmail.com>
2022-08-26 18:10:33 -0300

    Day41.swift

--- day41.swift
+++ day41.swift
@@ -33,4 +33,39 @@ você já pensou ser muito difícil e saber que pode fazê-lo.”
 Hoje você tem três tópicos para trabalhar, um dos quais é o seu desafio.
 
 
+---------- WHAT YOU LEARNED 
+
+Os projetos 7, 8 e 9 foram os primeiros da série que considero "difíceis": você teve que analisar dados JSON, teve que criar 
+um layout complexo para 7 Swifty Words e deu seus primeiros passos para criar código multithread - código que faz com que o 
+iOS faça mais de uma coisa de cada vez.
+
+None of those things were easy, but I hope you felt the results were worth the effort. And, as always, don’t worry if 
+you’re not 100% on them just – we’ll be using Codable and GCD more in future projects, so you’ll have ample chance to 
+practice.
+
+- Você já conheceu o UITabBarController, que é outro componente principal do iOS - você o vê na App Store, Música, iBooks, 
+Saúde, Atividade e muito mais.
+
+- Each item on the tab bar is represented by a UITabBarItem that has a title and icon. If you want to use one of Apple’s 
+icons, it means using Apple’s titles too.
+
+- We used Data to load a URL, with its contentsOf method. That then got fed to JSONDecoder so that we could read it in code.
+
+- We used WKWebView again, this time to show the petition content in the app. This time, though, we wanted to load our own 
+HTML rather than a web site, so we used the loadHTMLString() method.
+
+- Em vez de conectar muitas ações no Interface Builder, você viu como poderia escrever interfaces de usuário em código. 
+Isso foi particularmente útil para os botões de letra de 7 Swifty Words, porque poderíamos usar um loop aninhado.
+
+- In project 8 we used property observers, using didSet. This meant that whenever the score property changed, we automatically 
+updated the scoreLabel to reflect the new score.
+
+- You learned how to execute code on the main thread and on background threads using DispatchQueue, and also met the 
+performSelector(inBackground:) method, which is the easiest way to run one whole method on a background thread.
+
+- Finally, you learned several new methods, not least enumerated() for looping through arrays, joined() for bringing an 
+array into a single value, and replacingOccurrences() to change text inside a string.
+
+
+
 ---------- 
