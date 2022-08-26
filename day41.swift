89baab3031114097bad89a707a58920c6be074ec
Daniel Braga <dbragabarbosa@gmail.com>
2022-08-26 18:08:24 -0300

    Day41.swift

---
+++ day41.swift
@@ -0,0 +1,36 @@
+26/08/2022
+
+
+MARCO: PROJETOS 7 - 9
+
+
+
+Há alguma pseudociência que afirma que a segunda ou terceira segunda-feira de janeiro é a "segunda-feira azul" - o dia 
+mais deprimente do ano. As razões dadas incluem as condições climáticas no hemisfério norte sendo sombrias, a quantidade 
+de tempo desde o feriado de Natal, o número de pessoas que desistem das resoluções de Ano Novo e muito mais.
+
+É, é claro, bobagem, mas tem um grão de verdade: é fácil desanimar quando você está no meio de algo, porque a novidade 
+inicial passou e ainda há muito mais trabalho pela frente.
+
+É onde você está hoje. Você está a menos da metade dos 100 Dias de Swift, mas já está sendo solicitado a abordar tópicos 
+complicados em vários dias consecutivos - o nível de dificuldade está aumentando, o ritmo provavelmente parece um pouco 
+mais rápido e a quantidade de código que você está sendo solicitado a escrever também está aumentando.
+
+Eu sei que alguns dos dias que você enfrentou foram mais difíceis do que outros, e também sei que você provavelmente está 
+se sentindo cansado - você está desistindo de muito tempo para que isso aconteça. Mas quero encorajá-lo a continuar 
+avançando: você está quase no meio do caminho agora, e os aplicativos que agora é capaz de criar são genuinamente úteis - 
+você percorreu um longo caminho!
+
+Helpfully, today is another consolidation day, which is partly a chance for us to go over some topics again to make sure 
+you really understand them, partly a chance for me to dive into specific topics such as enumerated() and GCD’s 
+background/foreground bounce, and partly a chance for you to try making your own app from scratch.
+
+Como sempre, o desafio que você enfrentará está absolutamente dentro do seu nível de habilidade atual, e lhe dá a chance 
+de ver o quão longe você chegou para si mesmo. Ricky Mondello – um dos times que constrói o Safari na Apple – disse uma 
+vez: “uma das minhas coisas favoritas sobre engenharia de software, ou qualquer tipo de crescimento, é voltar a algo que 
+você já pensou ser muito difícil e saber que pode fazê-lo.”
+
+Hoje você tem três tópicos para trabalhar, um dos quais é o seu desafio.
+
+
+---------- 
