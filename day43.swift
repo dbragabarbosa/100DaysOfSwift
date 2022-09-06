56a2faf27fed2fa5d6c57e7ef442a6f1cc900d11
Daniel Braga <dbragabarbosa@gmail.com>
2022-09-05 23:45:12 -0300

    Day43.swift

--- day43.swift
+++ day43.swift
@@ -3,3 +3,24 @@
 
 PROJECT 10, PART TWO 
 
+Hoje vamos fazer algo tão simples, mas tão fundamentalmente importante para a experiência de desenvolvimento de 
+aplicativos: vamos adicionar fotos de usuário ao nosso aplicativo.
+
+Eu sei, é simples, certo? E o UIKit simplifica as coisas. Mas, como você verá, adicionar fotos de usuário ao seu 
+aplicativo faz algo importante: torna seu aplicativo o deles. Eles o personalizaram com os rostos e lugares que amam, 
+e isso dá vida a tudo.
+
+Com esse poder vem uma condição importante: quando eles nos permitem ler sua vida privada assim, você precisa levar 
+essa permissão de privacidade a sério e viver de acordo com ela. Algumas outras plataformas jogam rápido e solto 
+com a privacidade do usuário, mas no mundo do iOS você faria bem em viver pelas palavras de Valerie Plame: “A 
+privacidade é preciosa - acho que a privacidade é o último verdadeiro luxo. Ser capaz de viver sua vida como 
+quiser sem que todos comentem ou saibam.”
+
+Portanto, não o compartilhe fora do aplicativo sem a permissão expressa do usuário, não o coloque no seu servidor e 
+nem envie dados analíticos, a menos que seja homogeneizado e anônimo. Por favor, seja um bom cidadão do iOS!
+
+Today you have three topics to work through, and you’ll learn about UIImagePickerController, NSObject, and more.
+
+
+---------- Importing photos with UIImagePickerController
+
