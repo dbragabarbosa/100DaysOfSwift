1f40b4e426fb00c333aa383a3727d4e61c20c1aa
Daniel Braga <dbragabarbosa@gmail.com>
2022-09-15 22:33:25 -0300

    Day47.swift

--- day47.swift
+++ day47.swift
@@ -1,3 +1,21 @@
 12/09/2022
 
 
+Project 11, part three 
+
+
+Hoje chegamos ao final do projeto 11, o que significa que é hora de mais alguns desafios. Eu sei que a pista está bem 
+ali no nome, mas esses desafios são projetados para serem desafiadores - você precisa pensar sobre os problemas anteriores 
+que resolveu em outros projetos e, em seguida, aplicar esse conhecimento de uma maneira nova e diferente aqui.
+
+Esses desafios são projetados para ajudá-lo a aprender, porque uma coisa é seguir minhas instruções e outra é aplicar 
+seu conhecimento a novos problemas. No entanto, se você os achar complicados, não se preocupe se vir pessoas on-line 
+resolvendo-os rapidamente - elas estão aqui para ajudá-lo a aprender, não como algum tipo de maneira de se medir contra 
+os outros. Como Shakuntala Devi disse uma vez, “ninguém me desafia - eu me desafio”.
+
+Today you should work through the SKEmitterNode and wrap up chapters for project 11, complete its review, then work through 
+all three of its challenges.
+
+
+
+---------- 
