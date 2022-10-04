29d198d0ef2f481909e2cda85555a165902d1052
Daniel Braga <dbragabarbosa@gmail.com>
2022-10-04 12:52:51 -0300

    Day48.swift

--- Project10/Project10/Person.swift
+++ Project10/Project10/Person.swift
@@ -17,4 +17,17 @@ class Person: NSObject, NSCoding
         self.name = name
         self.image = image 
     }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+
+    func encode(with aCoder: NSCoder)
+    {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
 }
