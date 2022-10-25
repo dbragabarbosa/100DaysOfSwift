3a76a7ac10963b62c7cce389e621420a5ea519f6
Daniel Braga <dbragabarbosa@gmail.com>
2022-10-24 23:29:04 -0300

    Day50.swift

---
+++ day50.swift
@@ -0,0 +1,22 @@
+12/09/2022
+
+
+Milestone: Projects 10-12
+
+It’s time for another consolidation day, which means you have lots to review, lots to dig into, and a fresh programming 
+challenge too. I’ve mentioned before the importance of repetition, so I hope you’re able to seize this opportunity to try 
+your hand at something new that helps you apply your new knowledge practically.
+
+Aristotle once said, “it is frequent repetition that produces a natural tendency.” And that’s our goal with this 
+repetition: to bake knowledge of Swift and iOS so deep into your head and hands that you can start to build things 
+without second guessing yourself. So, when it comes to writing code for your own projects, or even writing code during 
+an iOS interview, you know for sure that you’ve got it in hand – you can do it, because you’ve done it 20 times before.
+
+So, make sure you work through today’s challenge fully: experiment and see what you can make!
+
+Today you have three topics to work through, one of which of is your challenge.
+
+
+
+---------- What you learned 
+
