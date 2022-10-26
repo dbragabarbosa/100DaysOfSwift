b0bbe0044054b613d96b71e8b5790045b25c47a7
Daniel Braga <dbragabarbosa@gmail.com>
2022-10-25 23:41:53 -0300

    Day51.swift

---
+++ day51.swift
@@ -0,0 +1,37 @@
+25/10/2022
+
+
+Expanding your horizons
+
+
+Today marks the start of the second half of this course – you’ve finished the first 50 days now, and it’s time for the 
+second 50 days. So, today we’re going to do something a little different: I want to give you a sense of the broader Swift 
+ecosystem.
+
+I often write about conferences, because I believe most people benefit from getting out there and meeting other people, 
+hearing fresh ideas, and playing an active role in the community.
+
+While you’re just learning it’s easy to miss just how interesting our community is, so today I would you like you to 
+watch two talks I’ve given at conferences last year: dotSwift in Paris and NSSpain in Logroño.
+
+I’ve picked out these talks for different reasons:
+
+- At this point in your learning, the dotSwift talk will be totally within your grasp, but will still inspire you to 
+write better Swift code.
+
+- The NSSpain talk will about 70% in your grasp, and about 30% new, but I think at this point you’ll appreciate some of 
+the many pain points I talk about.
+
+Don’t worry if there are some things you don’t fully understand yet. When you listen to a conference talk, very often 
+you’ll know some things already, learn a few things quickly, and have to do some extra research on others – that’s 
+normal, and it shows that it hit the sweet spot.
+
+As I write this, I spoke to someone just yesterday who said they attended a conference in 2018 and then again in 2019, 
+and it was amazing for them to see how much more they understood after a year of work. As Leonardo da Vinci said, 
+“the noblest pleasure is the joy of understanding,” and I can think of few better yardsticks of your progress than 
+watching talks live or recorded and realizing they are starting to make more sense than before.
+
+Hopefully this video day gives you a little time to catch your breath, while also giving you just a taste of what else 
+is out there.
+
+Today you have two videos to watch, and you’ll learn about map(), flatMap(), filter(), and more.
