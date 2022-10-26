b19bd79f965ce35f192333cdde758c243381f3c8
Daniel Braga <dbragabarbosa@gmail.com>
2022-10-26 17:46:13 -0300

    Day52.swift

--- day52.swift
+++ day52.swift
@@ -46,3 +46,64 @@ Para começar, crie um novo projeto de Aplicativo de Visualização Única no Xc
 
 
 
+---------- Designing the interface
+
+Selecione seu arquivo Main.storyboard para abrir o Interface Builder e, em seguida, incorpore o controlador de 
+visualização dentro de um controlador de navegação.
+
+Abra a biblioteca de objetos, depois pesquise por "UIView" e arraste uma visualização para o seu controlador - esta é uma 
+visualização regular, não um controlador de visualização ou uma referência de storyboard. Se o Interface Builder já 
+estiver usando o dimensionamento do iPhone 8, dê à nova visualização uma largura de 375 e uma altura de 470, com X:0 
+e Y:64. Se você não tiver certeza, procure as palavras "Visualizar como: iPhone 8" na parte inferior do Interface Builder 
+- se você vir outra coisa, clique nele e selecione iPhone 8 e Retrato.
+
+Tudo bem, usando 375x470 e X:0 Y:64 deve colocar a visão logo abaixo do controlador de navegação, ocupando a maior parte 
+da tela. No inspetor de atributos, dê à visualização a cor de fundo "Cor Cinza Escuro".
+
+Crie uma visualização de imagem e coloque-a dentro da visualização que você acabou de criar. Eu gostaria que você recuse 
+por 10 pontos em cada lado - ou seja, largura 355, altura 450, X:10, Y:10. Altere o modo de visualização da imagem de 
+"Escala para preencher" para "Aspect Fit". Não coloque mais visualizações dentro da visualização cinza - todo o resto 
+deve ser colocado diretamente na visualização principal (branca).
+
+Essa é a parte superior da interface do usuário completa. Para a parte inferior, comece criando uma etiqueta com 
+largura 72, altura 21, X:16, Y:562. Dê a ele o texto "Intensidade" e faça-o alinhado corretamente. Agora solte um 
+controle deslizante ao lado dele, dando-lhe largura 262, X:96, Y:558. Você não pode ajustar a altura dos controles 
+deslizantes, então deixe-a no padrão.
+
+Finalmente, coloque dois botões. O primeiro botão deve ter 120 de largura e 44 de altura, com X:16, Y:605. Dê a ele o 
+título "Alterar Filtro". O segundo botão deve ter 60 de largura por 44 de altura, com X:300, Y:605. Dê a ele o título 
+"Salvar".
+
+Na imagem abaixo, você pode ver como seu layout acabado deve ficar.
+
+Então esse é o layout básico completo, mas é claro que precisamos adicionar restrições de Layout Automático porque 
+precisamos de tudo para redimensionar sem problemas em vários dispositivos. Mas, você sabe, estou me sentindo preguiçoso 
+- que tal fazermos o Auto Layout fazer o trabalho para nós desta vez?
+
+Selecione o controlador de visualização clicando em "Visualizar Controlador" no esboço do documento, depois vá para o 
+menu Editor e escolha Resolver Problemas de Layout Automático > Redefinir para Restrições Sugeridas.
+
+É isso aí! Seu Layout Automático está pronto: o Xcode acabou de adicionar as restrições ideais em todos os lugares para 
+que sua interface seja redimensionada perfeitamente. Não acredita em mim? Tente dar à visualização da imagem uma cor de 
+fundo vermelha (temporariamente!), em seguida, inicie-a em qualquer dispositivo e gire a tela. Você deve ver tudo 
+(incluindo a caixa vermelha) ser posicionado e redimensionado corretamente.
+
+Certifique-se de mudar a visualização da imagem de volta para ter uma cor de fundo clara.
+
+Isso foi notavelmente fácil, e é outro exemplo de que a Apple está fazendo muito trabalho duro para você. Usar o Xcode 
+para fazer suas regras de Layout Automático pode ser uma ajuda real, mas não será certo o tempo todo. Afinal, ele só 
+faz o seu melhor palpite quanto às suas intenções. Ele também frequentemente adicionará mais restrições do que o 
+estritamente necessário para o trabalho, então use-o com cuidado.
+
+Before we leave Interface Builder, I'd like you to add an outlet for the image view and the slider, called respectively 
+imageView and intensity. Please also create actions from the two buttons, calling methods changeFilter() and save(). 
+You can leave these methods with no code inside them for now.
+
+Finalmente, queremos que a interface do usuário seja atualizada quando o controle deslizante for arrastado, então crie 
+uma ação a partir do controle deslizante. Deve dar a você o evento "Valor Alterado" em vez de Touch Up Inside, e é isso 
+que queremos. Chame o método da ação intensityChanged().
+
+É isso para o storyboard, então traga ViewController.swift e vamos começar a codificar...
+
+
+
