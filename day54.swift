cf7de909e5b2b952a5a1bc1cfbc16b2f775734df
Daniel Braga <dbragabarbosa@gmail.com>
2022-11-02 01:09:50 -0300

    Day54.swift

---
+++ day54.swift
@@ -0,0 +1,24 @@
+02/11/2022
+
+
+Project 13, part three
+
+A Core Image não é incrível? Nós só nos detávamos com um punhado de efeitos, mas mesmo assim pode fazer coisas em tempo 
+real que levaria semanas, se não meses, para codificar manualmente. Melhor ainda, ele automaticamente aproveita ao máximo
+a GPU e o Metal, para que você tenha o máximo desempenho garantido, mesmo que mal tenhamos escrito nenhum código.
+
+Espero que isso tenha lhe dado o gosto pela exploração. Os desafios que você estará completando hoje se concentram em 
+melhorar o que você tem, mas eu realmente encorajo você a procurar a lista de filtros Core Image e experimentar alguns 
+deles. É divertido, você aprenderá coisas e realmente incorporará seu novo conhecimento de Imagem Principal no fundo 
+do seu cérebro.
+
+Nate Berkus, um famoso designer de interiores americano, disse uma vez “você enriquecerá sua vida de forma imensurável 
+se se aproximar dela com uma sensação de admiração e descoberta, e sempre se desafiar a experimentar coisas novas”. 
+Bem, esta é a sua chance: entre no mundo da Core Image, permita-se ser brincalhão por um tempo e veja o que você pode criar!
+
+Hoje você deve trabalhar no capítulo de encerramento do projeto 13, concluir sua revisão e, em seguida, trabalhar em 
+todos os seus três desafios.
+
+
+
+---------- 
