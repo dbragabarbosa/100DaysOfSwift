d4d3a467998e35485ef75f95f88f6f33c52c93c0
Daniel Braga <dbragabarbosa@gmail.com>
2022-11-08 23:18:30 -0300

    Day55.swift

---
+++ day55.swift
@@ -0,0 +1,25 @@
+08/11/2022
+
+
+Project 14, part one 
+
+
+Ezra Koenig said “some people say video games rot your brain, but I think they work different muscles that maybe you don't
+normally use.” By now you should know I believe that idea extends further: making video games works different muscles 
+that you wouldn’t otherwise use making apps or similar.
+
+Yes, you might decide making games is what you want to do on iOS, and maybe you’ll even be lucky enough to get into 
+Apple’s Arcade program. Of course, you’re also just as likely to decide that your goal is to make apps, but even then 
+the skills you learn making games will prove useful.
+
+In this project, the skills include loops, masking, GCD, and more – all the kinds of things you can use regardless of 
+whether you decide games are for you. And of course the deeper skill is learning how to structure Swift to solve problems 
+– practicing the art of understanding code flow across larger projects.
+
+So, whether or not you decide to make games in the future, there’s lots to learn in this project!
+
+Today you have three topics to work through, and you’ll learn about SKCropNode, SKTexture, and more.
+
+
+
+---------- 
