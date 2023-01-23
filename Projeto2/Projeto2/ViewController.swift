35dbbf08ac4f62d733bb78b74e1fbecdbe3324b0
Daniel Braga <dbragabarbosa@gmail.com>
2023-01-23 19:54:18 -0300

    :construction: Projetando o layout

---
+++ Projeto2/Projeto2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projeto2
+//
+//  Created by Daniel Braga Barbosa on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    
+    @IBOutlet var button2: UIButton!
+    
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
