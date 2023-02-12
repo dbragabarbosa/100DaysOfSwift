92b269532ee5994317ce260b4c716f4a0a66ccd9
Daniel Braga <dbragabarbosa@gmail.com>
2023-02-11 23:03:46 -0300

    :construction: Consumindo o JSON com o protocolo Codable

---
+++ Projeto7/Projeto7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Projeto7
+//
+//  Created by Daniel Braga Barbosa on 11/02/23.
+//
+
+import Foundation
+
+struct Petition: Codable
+{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
