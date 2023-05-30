8940112ded4810e3fca36694a44db08200c23eab
Daniel Braga <dbragabarbosa@gmail.com>
2023-05-29 23:30:26 -0300

    day58

---
+++ day58.swift
@@ -0,0 +1,18 @@
+Project 15, part two
+
+
+I hope you can agree that animation in iOS is almost invisible – we just lay out all the changes we want to happen, and 
+Core Animation figures out all the intermediate steps to make it happen.
+
+As I’ve said several times, animation is more than about making our software look good – it has a functional purpose too, 
+ensuring that users understand why things and changing and what states they are moving from and to.
+
+Larry Niven once said, “that's the thing about people who think they hate computers – what they really hate is lousy 
+programmers.” Folks paid a lot of money for their iPhones and iPads, and Apple’s software oozes polish and refinement – 
+if you don’t put a similar level of care into your own code then your app will stick out, and not in a good way.
+
+So, with today’s challenges you need to apply your new animation knowledge to the projects you’ve made previously. 
+I think you’ll be pleased how easy it is, and I hope also inspired to take on more animations in the future!
+
+Today you should work through the wrap up chapter for project 15, complete its review, then work through all three of 
+its challenges.
