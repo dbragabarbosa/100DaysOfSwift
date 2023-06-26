392e3c5138330ca64791753e21377fa3f8b9769e
Daniel Braga <dbragabarbosa@gmail.com>
2023-06-26 10:02:33 -0300

    day68

---
+++ day68.swift
@@ -0,0 +1,15 @@
+Project 19, part two
+
+Yesterday we did a lot of set up work, but now it’s time for the pay off: we’re going to send custom JavaScript code back 
+to Safari and have it executed, giving our (hypothetical!) users the ability to manipulate web pages right on their phone.
+
+I’m not going to pretend that Apple’s actions API is pleasant, and I’m certainly not going to argue that you need to like 
+using JavaScript. As Jeremy Foster once said, “every developer loves and hates JavaScript a little bit” – it’s something 
+we all need to be aware of, and perhaps even be able to write to some level, but it’s by no means required.
+
+The point of this project isn’t to teach you JavaScript, but instead to show you what’s possible with iOS. 
+The overwhelming majority of iPhone owners use Safari, and in just two days you’ll have finished building an app that 
+adds functionality directly to it – what common problems can you solve?
+
+Today you have three topics to work through, and you’ll learn about UITextView, NotificationCenter, and more.
+
