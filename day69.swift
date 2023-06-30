1b57b7b3190b78fee1b977a7f7890abc7c088dbe
Daniel Braga <dbragabarbosa@gmail.com>
2023-06-30 09:45:05 -0300

    day69

---
+++ day69.swift
@@ -0,0 +1,14 @@
+Project 19, part three
+
+You’ve modified Safari! Apple’s web browser has been there since the very first iPhone, and now you’ve hooked your own 
+code directly into it. You’ll be taking on a few challenges today to encourage you to think of ways to use your new-found 
+power, but I hope you can immediately realize the potential here – even a small improvement can make a difference to 
+millions of people.
+
+I know this project was hard, but I have some bad news for you: tomorrow’s game is also hard. But as Kimberly Guilfoyle 
+said, “through determination and self-focus and discipline, you can accomplish anything.”
+
+Stick with it! Things get easier after the next game, but your skillset continues to expand rapidly.
+
+Today you should work through the wrap up chapter for project 19, complete its review, then work through all three of 
+its challenges.
