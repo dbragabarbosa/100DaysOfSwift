0bec07243574f9cf4c608c2ea6a3658fd48143fd
Daniel Braga <dbragabarbosa@gmail.com>
2023-07-11 10:59:28 -0300

    day71

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -18,11 +18,12 @@ class GameScene: SKScene
     let rightEdge = 1024 + 22
     
     var gameScore: SKLabelNode!
-    var score = 0 {
-        didSet {
-            gameScore.text = "Score: \(score)"
-        }
-    }
+    var score = 0
+//    {
+//        didSet {
+//            gameScore.text = "Score: \(score)"
+//        }
+//    }
     
     override func didMove(to view: SKView)
     {
@@ -163,6 +164,51 @@ class GameScene: SKScene
                 fireworks.remove(at: index)
                 firework.removeFromParent()
             }
+        }
+    }
+    
+    
+    func explode(firework: SKNode)
+    {
+        if let emitter = SKEmitterNode(fileNamed: "explode")
+        {
+            emitter.position = firework.position
+            addChild(emitter)
+        }
+        
+        firework.removeFromParent()
+    }
+    
+    func explodeFireworks()
+    {
+        var numExploded = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed()
+        {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            
+            if firework.name == "selected"
+            {
+                explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numExploded += 1
+            }
+        }
+        
+        switch numExploded
+        {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
         }
     }
     
