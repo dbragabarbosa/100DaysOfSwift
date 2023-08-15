a182489cf07e7bd6ede8ec201fb516066ceea43d
Daniel Braga <dbragabarbosa@gmail.com>
2023-08-15 19:58:10 -0300

    day91

---
+++ day91.swift
@@ -0,0 +1,34 @@
+Core Graphics redux
+
+Fred Donaldson has spent the last 40 years researching how and why children play, has taught at three US universities, 
+written multiple books, and created a foundation to help teach his learning approach to the wider world.
+
+One of the quotes he’s most famous for is this one: “Children learn as they play. Most importantly, in play children learn 
+how to learn.” That’s huge, if you stop and think about it: play helps teach kids the thing they are working with, but 
+also deepens in them the concepts underlying all of learning itself.
+
+Of course, play doesn’t apply just to kids – as George Bernard Shaw said, “we don’t stop playing because we grow old; we 
+grow old because we stop playing.” And so today’s second consolidation day is geared around giving you the space and time 
+to play around with Core Graphics to see what you can do.
+
+I’ve created a special Swift playground that walks you through a variety of Core Graphics techniques. However, I’ve made 
+two versions, and it’s important you choose the right one.
+
+Today you should work through my Learn Core Graphics playground, either in Swift Playgrounds (preferred) or Xcode.
+
+Remember: experimentation like this is a powerful way to learn Core Graphics, but also learning how to learn in general. 
+Enjoy it!
+
+If you have an iPad
+Please install Apple’s “Swift Playgrounds” app from the App Store. It’s free, and allows you to experience my Core 
+Graphics playground in its fullest.
+
+Once you have Swift Playgrounds installed on your iPad, visit https://www.hackingwithswift.com/playgrounds on your iPad 
+and click the big green “CLICK TO SUBSCRIBE” button. That should subscribe you to my source of Swift playgrounds.
+
+Finally, select my Learn Core Graphics playground and dive in.
+
+If you do not have an iPad
+I’ve made a special version of my Learn Core Graphics playground that works in Xcode 10.2 on your Mac. It doesn’t have 
+all the same features because Xcode’s playground support isn’t as extensive as the Swift Playgrounds app, but it should 
+still work great.
