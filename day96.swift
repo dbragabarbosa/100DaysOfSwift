9af5577a7264bb97ef711d67983aadc92fd62b91
Daniel Braga <dbragabarbosa@gmail.com>
2023-08-28 09:21:00 -0300

    day96

---
+++ day96.swift
@@ -0,0 +1,41 @@
+Project 29, part three
+
+This game takes techniques you’ve learned previously – not least SpriteKit, physics, and Core Graphics – and stitches 
+them together into something new, something clever, and something fun.
+
+This says a lot about the amount of progress you’ve made: you ought to be able to go through every line of code in the 
+project and explain what its purpose is, which would have been impossible a few months ago. But it also gets right to the 
+heart of programming: it’s a creative process - we’re not just flipping bits in a CPU and hoping for the best, but using 
+the skills we’ve learned to design, to innovate, and to bring something new into the world.
+
+Craig Federighi, senior vice president of software engineer at Apple, once said “people sometimes have a view of 
+programming that is something solitary and very technical, but programming is among the most creative, expressive, 
+and social careers.”
+
+As we approach the end of this course, I hope you’ll keep those words in mind. As I’ve said countless times before: 
+programming is an art, so don’t spend all your time sharpening your pencil when you should be drawing.
+
+Today you should work through the wrap up chapter for project 29, complete its review, then work through all three of 
+its challenges.
+
+
+
+---------- Wrap up
+
+This was a huge project, and gave you lots to learn about mixing UIKit and SpriteKit, texture atlases, scene transitions, 
+and of course destructible terrain – while also giving you another real-world project under your belt.
+
+This project should also have shown you how you can bring skills together to make something bigger and better: Core 
+Graphics and physics combined to make destructible terrain, and really it wasn’t that hard!
+
+Challenge
+One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try 
+your new knowledge to make sure you fully understand what’s going on:
+
+1. Add code and UI to track the player scores across levels, then make the game end after one player has won three times.
+
+2. Add Auto Layout rules for the UI components in our storyboard, allowing them to remain positioned neatly regardless 
+of which iPad size is used.
+
+3. Use the physics world’s gravity to add random wind to each level, making sure to add a label telling players the 
+direction and strength.
